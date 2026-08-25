f2567e2dbb12d74de69b5921e663e80af895ef09
PierrotLL <pierrotll89@hotmail.fr>
2026-08-25 22:05:12 +0200

    Add files via upload

---
+++ pwaInstallButton.js
@@ -0,0 +1,20 @@
+function isRunningStandalone() {
+    return matchMedia('(display-mode: standalone)').matches;
+}
+
+onload = _=> {
+	if (isRunningStandalone()) return;
+	installButton = document.createElement("button");
+	installButton.innerHTML = "&#128242; Install";
+	installButton.style = "position:absolute; top:0; right:0; margin:1em; opacity:0; transition:1s;";
+	
+	addEventListener("beforeinstallprompt", e=> {
+		document.body.append(installButton);
+		requestFrameAnimation(_=> installButton.style.opacity = 1);
+		installButton.onclick = _=> {
+			e.prompt().then(value => {
+				if (value == "accepted") installButton.remove();
+			});
+		};
+	});
+}
