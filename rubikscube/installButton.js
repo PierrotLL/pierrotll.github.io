6dd281fda6d87d7859401c3d968608d50d91b995
PierrotLL <pierrotll89@hotmail.fr>
2026-08-25 21:57:31 +0200

    Add files via upload

--- rubikscube/installButton.js
+++ rubikscube/installButton.js
@@ -1,4 +1,9 @@
+function isRunningStandalone() {
+    return matchMedia('(display-mode: standalone)').matches;
+}
+
 onload = _=> {
+	if (isRunningStandalone()) return;
 	installButton = document.createElement("button");
 	installButton.innerHTML = "&#128242; Install";
 	installButton.style = "display:none; position:absolute; top:0; right:0; margin:1em; opacity:0; transition:1s;";
@@ -7,6 +12,10 @@ onload = _=> {
 	addEventListener("beforeinstallprompt", e=> {
 		installButton.style.display = "";
 		requestFrameAnimation(_=> installButton.style.opacity = 1);
-		installButton.onclick = e.prompt();
+		installButton.onclick = _=> {
+			e.prompt().then(value => {
+				if (value == "accepted") installButton.remove();
+			});
+		};
 	});
 }
