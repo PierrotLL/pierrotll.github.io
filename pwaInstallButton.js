e16615a1c7c87b7b6190b2f831cd52eec26fee28
PierrotLL <pierrotll89@hotmail.fr>
2026-08-26 14:33:20 +0200

    Add files via upload

--- pwaInstallButton.js
+++ pwaInstallButton.js
@@ -4,14 +4,16 @@
 	installButton.setAttribute("id", "installButton");
 	installButton.innerHTML = "&#128242; Install";
 	installButton.style = "position:absolute; top:0; right:0; font-size: 16px; padding:8px 16px; margin:1em; opacity:0; transition:1s; ";
-	installButton.onclick = (_=>{
-		installPromptEvent && installPromptEvent.prompt().then(value => {
-			if (value.outcome == "accepted") installButton.remove();
-		});
-	});
+	let installPromptEvent;
 	addEventListener("beforeinstallprompt", e=> {
 		installPromptEvent = e;
 		document.body.append(installButton);
 		requestAnimationFrame(_=> installButton.style.opacity = 1);
+	});
+	installButton.onclick = (_=>{
+		installPromptEvent.prompt().then(value => {
+			if (value.outcome == "accepted") installButton.remove();
+			else installPromptEvent.userChoice = undefined;
+		});
 	});
 })();
