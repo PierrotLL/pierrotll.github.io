2ec29ec5d5444c5082423e7684d3f4fa6f80cb7e
PierrotLL <pierrotll89@hotmail.fr>
2026-08-30 07:40:48 +0200

    Add files via upload

--- rubikscube/rubikscube-sw.js
+++ rubikscube/rubikscube-sw.js
@@ -1,18 +1,21 @@
-const cacheName = "rubikscube-2026-08-29-v6";
+const cacheName = "rubikscube-2026-08-30";
 const contentToCache = [
 	"rubikscube.html",
 	"rubikscube.webmanifest",
 	"icon-64.webp",
+	"icon-192.png",
 	"icon-512.png",
 ];
 
 self.addEventListener("install", e => {
+	console.log(cacheName, "install");
 	e.waitUntil(
 		caches.open(cacheName).then(cache => cache.addAll(contentToCache))
 	);
 });
 
 self.addEventListener("activate", e => {
+	console.log(cacheName, "activate");
 	const appName = cacheName.replace(/-.*/,"");
 	e.waitUntil(
 		caches.keys()
@@ -24,8 +27,14 @@ self.addEventListener("activate", e => {
 });
 
 self.addEventListener("fetch", e => {
+	console.log(cacheName, "fetch", e.request.URL);
 	e.respondWith(
 		caches.match(e.request)
-		.then(cacheResponse => cacheResponse || fetch(e.request))
+		//.then(cacheResponse => cacheResponse || fetch(e.request))
+		.then(cacheResponse =>
+			(console.log(cacheName, "fetch response from cache", e.request.URL), cacheResponse)
+			||
+			fetch(e.request).then(r=>(console.log(cacheName, "fetch loaded", e.request.URL),r),r=>(console.log(cacheName, "fetch load error", e.request.URL, r),null))
+		)
 	);
 });
